07fdd4b92bb8c3eb926e58e7c8bf845c72a8e762
Kirt Asia Mag-usara <162999637+Miigurd@users.noreply.github.com>
2024-05-31 11:24:15 +0800

    new file:   images/BG.png new file:   images/airbnb.jpg new file:   images/arrow.png new file:   images/beach.jpg new file:   images/carrent.jpg new file:   images/land.jpg new file:   images/olongapo.jpg new file:   images/travel-plane.jpg new file:   index.html new file:   indexstyle.css new file:   javsc.js new file:   stays.html new file:   styles.css

---
+++ javsc.js
@@ -0,0 +1,30 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
